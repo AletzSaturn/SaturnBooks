d41b4a9371ac7b978c6932e9c21da7383a0ae10a
Alejandro Arellano <anecandro1@gmail.com>
2025-09-01 11:11:17 -0600

    Commented auth provider

--- src/sign-in/AuthProvider.context.ts
+++ src/sign-in/AuthProvider.context.ts
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+// import { createContext, useState } from "react";
 
-const [user, setUser] = useState(null);
-export const AuthContext = createContext(1);
+// const [user, setUser] = useState(null);
+// export const AuthContext = createContext(1);
